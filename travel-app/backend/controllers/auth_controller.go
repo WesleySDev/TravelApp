b78e8753d94b2ab96339c6447173948b83069a00
WesleySDev <wesleydasilvagit@gmail.com>
2026-02-24 20:21:02 -0300

    feat: Adicionar estrutura de solicitação de registro no controlador de autenticação

---
+++ travel-app/backend/controllers/auth_controller.go
@@ -0,0 +1,7 @@
+package controllers
+
+type RegisterRequest struct {
+	Name     string `json:"name" binding:"required,min=`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6"`
+}
