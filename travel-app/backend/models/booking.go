7085bf19f44cbc0b56691895b57e11ba6db12205
WesleySDev <you@example.com>
2026-04-05 20:56:53 -0300

    feat: Adicionar campos de status e relacionamentos no modelo Booking

--- travel-app/backend/models/booking.go
+++ travel-app/backend/models/booking.go
@@ -6,5 +6,10 @@ type Booking struct {
 	gorm.Model
 
 	UserID uint `gorm:"not null"`
-	//continua...
+	User User `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+
+	PackageID uint `gorm:"not null"`
+	Package Package `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+
+	Status string `gorm:"type:text;default:'pending';check:status IN ('pending', 'confirmed', 'cancelled')"`
 }  
